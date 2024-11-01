c282dcb8c424712ebc1c940642d292fdfa74707c
Hanif Carroll <hanifcarroll@gmail.com>
2024-11-01 18:35:13 -0300

    Move the delete/new correction button

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,30 +9,32 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="text-3xl font-bold mb-4">{{ $post->title }}</h1>
-                    <p class="text-sm text-gray-600 dark:text-gray-400 mb-4">
+                    <div class="flex justify-between">
+                        <h1 class="text-xl font-bold mb-1">{{ $post->title }}</h1>
+                        <div class="">
+                            @if($post->user_id !== auth()->id())
+                                <x-primary-button href="{{ route('posts.corrections.create', $post) }}">
+                                    New Correction
+                                </x-primary-button>
+                            @endif
+                            @if($post->user_id === auth()->id())
+                                <form action="{{ route('posts.destroy', $post) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this post?');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="inline-flex items-center px-4 py-2 border border-red-600 rounded-md font-semibold text-xs text-red-600 uppercase tracking-widest hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                        Delete Post
+                                    </button>
+                                </form>
+                            @endif
+                        </div>
+                    </div>
+                    <p class="text-sm text-gray-600 dark:text-gray-400 mb-8">
                         By {{ $post->user->name }} | {{ $post->created_at->format('F j, Y, g:i a') }}
                     </p>
                     <div class="prose dark:prose-invert max-w-none">
                         {!! nl2br(e($post->text)) !!}
                     </div>
 
-                    <div class="mt-6 flex">
-                        @if($post->user_id !== auth()->id())
-                            <x-primary-button href="{{ route('posts.corrections.create', $post) }}">
-                                New Correction
-                            </x-primary-button>
-                        @endif
-                        @if($post->user_id === auth()->id())
-                            <form action="{{ route('posts.destroy', $post) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this post?');">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="inline-flex items-center px-4 py-2 border border-red-600 rounded-md font-semibold text-xs text-red-600 uppercase tracking-widest hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                                    Delete Post
-                                </button>
-                            </form>
-                        @endif
-                    </div>
                 </div>
             </div>
             <div class="mt-8">
