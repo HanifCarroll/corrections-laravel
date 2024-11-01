c7f251a016283390cd60599d6cb1aa190354e1e1
Hanif Carroll <hanifcarroll@gmail.com>
2024-11-01 18:22:56 -0300

    Update styles on post preview card

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,28 +7,26 @@
         </div>
     </x-slot>
 
-    <div>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @foreach ($posts as $post)
-                        <div class="mb-4 pb-4 border-b border-gray-200 dark:border-gray-700">
+    <div class="px-6">
+        <div class="max-w-7xl">
+            <div class="text-gray-900 dark:text-gray-100">
+                @foreach ($posts as $post)
+                    <a href="{{ route('posts.show', $post) }}" class="hover:underline">
+                        <div class="bg-white rounded-lg p-4 mb-6">
                             <h3 class="text-lg font-semibold">
-                                <a href="{{ route('posts.show', $post) }}" class="hover:underline">
-                                    {{ $post->title }}
-                                </a>
+                                {{ $post->title }}
                             </h3>
-                            <p class="text-sm text-gray-600 dark:text-gray-400">
+                            <p class="text-sm text-gray-600 dark:text-gray-400 mb-4">
                                 By {{ $post->user->name }} | {{ $post->created_at->diffForHumans() }}
                             </p>
                             <p class="mt-2">
                                 {{ Str::limit($post->text, 150) }}
                             </p>
                         </div>
-                    @endforeach
+                    </a>
+                @endforeach
 
-                    {{ $posts->links() }}
-                </div>
+                {{ $posts->links() }}
             </div>
         </div>
     </div>
